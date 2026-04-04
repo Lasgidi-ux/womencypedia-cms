be65490ee746db31cad9dc9246e5c6ca98c85286
mejire madudu <mejiremadudu@yahoo.com>
2026-04-04 19:49:29 +0100

    Enable public access to API endpoints by configuring routes
    - Added empty policies and middlewares arrays to biography, homepage, and collection routes
    - This allows public (unauthenticated) access to /api/biographies, /api/homepage, and /api/collections
    - Fixes 403 Forbidden errors for public API calls
    - Maintains admin panel authentication requirements

--- src/api/biography/routes/biography.ts
+++ src/api/biography/routes/biography.ts
@@ -1,4 +1,15 @@
 // @ts-nocheck
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::biography.biography');
+export default factories.createCoreRouter('api::biography.biography', {
+  config: {
+    find: {
+      policies: [],
+      middlewares: [],
+    },
+    findOne: {
+      policies: [],
+      middlewares: [],
+    },
+  },
+});
--- src/api/collection/routes/collection.ts
+++ src/api/collection/routes/collection.ts
@@ -1,4 +1,15 @@
 // @ts-nocheck
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::collection.collection');
+export default factories.createCoreRouter('api::collection.collection', {
+  config: {
+    find: {
+      policies: [],
+      middlewares: [],
+    },
+    findOne: {
+      policies: [],
+      middlewares: [],
+    },
+  },
+});
